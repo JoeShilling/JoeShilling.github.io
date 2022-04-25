6a05e5e5661e2331ec08211d82827f0aad43e352
Joe Shilling <57063098+JoeShilling@users.noreply.github.com>
2022-04-25 16:55:43 +0100

    basis for sequencer

--- code-sketches/2204/25/sketch.js
+++ code-sketches/2204/25/sketch.js
@@ -1,4 +1,36 @@
 let penSound, penImage;
+let shapes = [];
+
+class shape {
+    constructor(x,y,image,width,height, clickFunction=clicked) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.width = width;
+        this.height = height;
+        this.clickFunction = clickFunction;
+    }
+    
+    beenClicked(x,y) { //has this shape been clicked?
+        if (x >= this.x && x <= (this.x + this.width)) { //this if statements are seperate because i find it easier to read
+            if (y >= this.y && this.y <= (this.y + this.height)) {
+                             
+                this.width+=1;
+                this.clickFunction();
+                
+            }
+        }
+    }
+    
+    printImage(x = this.x, y = this.y, w = this.width, h = this.height) {
+        image(this.image, x, y, w, h);
+    }
+}
+
+function clicked() { //default clicked function
+    console.log('shape clicked');
+}
+
 
 function preload() {
     soundFormats('mp3');
@@ -8,12 +40,30 @@ function preload() {
 
 function setup() {
     let cnv = createCanvas(windowWidth,windowHeight);
+
+    shapes.push(new shape(100,100, penImage, 100,100, pen));
+    shapes.push(new shape(600,100, penImage, 100,100, pen));
+    //penImage.mouseClicked(pen);
     
-    let i = image(penImage, 200,200);
-    i.mousePressed(penSound.play());
+    cnv.mouseClicked(canvasClicked);
     
 }
 
 function draw() {
-  // put drawing code here
+  
+    for (let i in shapes) {
+        shapes[i].printImage();
+    }
+    
 }
+
+function canvasClicked(){
+    for (let i in shapes) {
+        shapes[i].beenClicked(mouseX,mouseY);
+    }
+}
+
+function pen() {
+    penSound.play();
+}
+
