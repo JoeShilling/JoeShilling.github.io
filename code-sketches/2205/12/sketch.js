9467f557f407fbf5cb1c5deb44e56efcd9e2d3df
Joe Shilling <57063098+JoeShilling@users.noreply.github.com>
2022-05-12 13:31:02 +0100

    prey & herbage

--- code-sketches/2205/12/sketch.js
+++ code-sketches/2205/12/sketch.js
@@ -5,48 +5,136 @@ let herbages = [];
 let preys = [];
 let predators = [];
 
+
+
 function setup() {
     cnv = createCanvas(windowWidth, windowHeight);
-    frameRate(30);
-    for (let x = windowWidth/12; x < windowWidth; x += windowWidth/4) {
-        for (let y = windowHeight/12; x < windowHeight; y += windowWidth/4) {
+    frameRate(120);
+    
+
+    let unit = windowWidth/12;
+    for (let x = unit; x <= windowWidth; x += unit) {
+        for (let y = unit; y <= windowHeight; y += unit) {
             herbages.push(new herbage(x, y, 120));
         }
     }
     
+    herbages.push(new herbage (200,200));
+    herbages.push(new herbage (400,600));
+    herbages.push(new herbage (800,100));
+    
+    preys.push(new prey (50,50));
+    preys.push(new prey (900,900));
 }
 
 function draw() {
-    for (let herb in herbages) {
+    //background('white');
+    
+    for (let herb of herbages) {
+        herb.update();
         herb.show();
     }
+    
+    for (let prey of preys) {
+        prey.update();
+        prey.show();
+    }
+    
 }
+
 
 class predator {
     constructor() {}
 }
 
+
 class prey {
-    constructor() {}
+    constructor(x, y) {
+        this.pos = createVector(x,y);
+        this.speed = 2;
+        this.hunger = 50; //when reaches 0 it dies, rip
+    }
+    
+    update() {
+        
+        if (this.hunger >= 109) {
+            preys.push(new prey(this.pos.x, this.pos.y));
+            this.hunger = 70;
+        }
+        
+        if (this.hunger > 0) {
+            this.hunger -= 0.3;
+        }
+        
+        if (this.hunger <= 0) {
+            console.log('dead');
+        } else {
+            let direction = createVector(0,0);
+            let distance = 10000000000000000;
+
+
+
+            //moving towards herbs
+            for (let herb of herbages) {
+                if (herb.growth >= 1) {
+                    if (this.pos.dist(herb.pos) < distance) {
+                        distance  = this.pos.dist(herb.pos);
+                        direction = p5.Vector.sub(herb.pos,  this.pos)
+
+                        if (direction.mag() > 1.5) { //so this probably shouldnt be here but we access to which herb the prey is touching
+                            direction.normalize();
+                            direction.mult(this.speed);
+
+                        } else {
+                            this.hunger += 25;
+                            herb.growth = 0;
+                        }
+
+
+                    }
+                }
+
+            }
+
+
+
+
+
+            this.pos.add(direction);
+        }
+        
+
+        
+    }
+    
+    show() {
+        rectMode(RADIUS);
+        colorMode(HSB)
+        let colour = color(200, this.hunger, 80);
+
+        fill(colour);
+        rect(this.pos.x, this.pos.y, 15);
+    }
 }
 
 class herbage {
-    constructor (x, y, regrowTime) { //xy is position, regrow is number of frames required to regrow
-        this.x = x;
-        this.y = y;
-        this.regrowTime = regrowTime;
+    constructor (x, y) { //xy is position, regrow is number of frames required to regrow
+        this.pos = createVector(x,y);
+        this.regrowTime = 1500; //frames required to regrow
         this.growth = 1; //shows state of growth
     }
     
-    update () {
+    update() {
         if (this.growth < 1) {
             this.growth += 1 / this.regrowTime;
         }
     }
     
-    show () {
+    show() {
         fill('#32a852');
-        ellipse(this.x, this.y, this.growth * 30);
+        noStroke();
+        ellipse(this.pos.x, this.pos.y, this.growth * 20);
     }
     
 }
+
