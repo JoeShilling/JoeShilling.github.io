6fae6d1769b46cc717dce3f0de550743bce51c64
Joe Shilling <57063098+JoeShilling@users.noreply.github.com>
2022-05-25 14:10:51 +0100

    Update sketch.js

--- code-sketches/2205/25/sketch.js
+++ code-sketches/2205/25/sketch.js
@@ -4,8 +4,10 @@
 //TRANSPARENCY IS A LIE
 let img;
 let tracks = [];
-let step=2;
+let step=3;
 let cam;
+let allSprites = [];
+
 
 function preload() {
     img = loadImage("tree1.png");
@@ -14,28 +16,6 @@ function preload() {
 function setup() {
     createCanvas(1000, 1000, WEBGL);
     
-    //randomly place tracks
-    let trackNum = 12;
-    for (let i = 0; i < trackNum; i++) {
-        print("Finding track position");
-        let positionFound = false;
-        let pos;
-        while (positionFound == false) { //bunch of restrictions on where a track can be placed
-            //we want to reduce overlap and keep a clear path in the middle
-            positionFound = true;
-            pos = getRndInteger(-1000,1001);
-            if (pos <= 150 && pos >= -100) { //keep middle path clear
-                positionFound = false;
-            } else {
-                for (let track of tracks) { //no overlaps
-                    if (Math.abs(pos - track.xPos) <= 50) {
-                        positionFound = false;
-                    }
-                }
-            }
-        }
-        tracks.push(new spriteTrack(pos,3));
-    }
     frameRate(60);
     cam = createCamera();
 }
@@ -44,19 +24,17 @@ function draw() {
     background("grey");
     
     cam.lookAt(0,0,0);
-    cam.setPosition(0,-200,3000);
+    cam.setPosition(0,-170,5000);
     
     //image(img,10,10, 10);
     
     //print all sprites from further to closest
-    let allSprites = [];
-    for (let track of tracks) {
-        for (let sprite of track.sprites) {
-            allSprites.push(sprite);
-        }
-        //track.show();
-        track.update();
+    
+    if (getRndInteger(0, 1000 - allSprites.length) > 900) { //chance of adding new trees
+        addSprite(img);
     }
+
+    updateSprites();
     
     for (let i = 0; i < allSprites.length; i++) { //BUBBLE SORT BBY!!
         for (let j = 0; j < (allSprites.length - i - 1); j++) {
@@ -71,38 +49,38 @@ function draw() {
     for (let sprite of allSprites) {
         sprite.show();
     }
-    
-    /*
-    for (let track of tracks) {
-        track.show();
-        track.update();
-    } */
+
 }
 
-class spriteTrack { //holds sprites and moves them along the 'track'
-    constructor(xPos, step) {
-        this.xPos = xPos;
-        this.sprites = [];
-        this.step = step;
-        
-    }
-    
-    update() {
-        for (let i = 0; i < this.sprites.length; i++) {
-            this.sprites[i].zPos += this.step;
-            if (this.sprites[i].zPos > 3020) { //distance limit
-                this.sprites.splice(0,1);
+
+function updateSprites() {
+        for (let i = 0; i < allSprites.length; i++) {
+            allSprites[i].zPos += step;
+            if (allSprites[i].zPos > 5020) { //distance limit
+                allSprites.splice(i,1);
             }
         }
-        if (getRndInteger(0, 1000 - this.sprites.length) > 970) { //chance of adding new trees
-            this.addSprite(img);
+        if (getRndInteger(0, 1000 - allSprites.length) > 670) { //chance of adding new trees
+            addSprite(img);
         }
     }
-    
-    addSprite(image) {
-        this.sprites.push(new sprite(image, 0, this.xPos));
+
+function addSprite(image) {
+    allSprites.push(new sprite(image, 0, getSpriteXpos()));
+}
+
+function getSpriteXpos() {
+    let positionFound = false;
+    let pos;
+    while (positionFound == false) { //bunch of restrictions on where a track can be placed
+        //we want to reduce overlap and keep a clear path in the middle
+        positionFound = true;
+        pos = getRndInteger(-1500,1501);
+        if (pos <= 150 && pos >= -100) { //keep middle path clear
+            positionFound = false;
+        }
     }
-    
+    return(pos);
 }
 
 class sprite {
